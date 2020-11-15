5a4cea570b2532f70145a334e6eec69aa8e7d93b
furkan116 <51910163+furkan116@users.noreply.github.com>
2020-11-15 12:41:40 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MyQueue customers = new CrazyMarket(300);
-        customers.yazdir(300);
-    }
-}
